56c24c08c72e2e0016965681a8e20c8ae4f4bcb1
Jay Freeman <bardo@Jays-iMac.local>
2017-01-20 14:23:52 -0800

    update and improve html, scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,29 @@
-function pingAndPong(input)
+function pingAndPong(input){
+  var output = [];
+  for (var i=1; i <= input; i++) {
+    if (i % 15 === 0) {
+      output.push("ping-pong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
+    } else {
+      output.push(i);
+    }
+  }
+  return output;
+}
+
 
 
 $(document).ready(function() {
-  $("#input").submit(function(event) {
+  $("#forminput").submit(function(event) {
     event.preventDefault();
     var input = $("#input").val();
     var output = pingAndPong(input);
     output.forEach(function(heyNow) {
-      $("#place").append("<ul>" + heyNow + "</ul>")
-    }
-
-
-
-
-      return output;
+      $(".place").append("<li>" + heyNow + "</li>");
+      $("#forminput")[0].reset();
+    });
   });
 });
